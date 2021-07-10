31d5c4dca86675b1ada6d64d2f23a22c0d88ca9c
Daniel Schulz <faulolio@gmail.com>
2021-07-10 13:37:52 +0200

    Tweak avatar jump

--- Assets/Scripts/Player/AvatarSettings.cs
+++ Assets/Scripts/Player/AvatarSettings.cs
@@ -9,8 +9,22 @@ namespace Runtime.Player {
         public float maxSpeed = 10;
         [SerializeField, Range(0, 10)]
         public float smoothingTime = 1;
+
+        [Header("Jumping")]
         [SerializeField, Range(0, 100)]
-        public float jumpSpeed = 10;
+        public float jumpStartSpeed = 10;
+        [SerializeField, Range(0, 100)]
+        public float jumpStopSpeed = 2;
+        [SerializeField, Range(0, 1)]
+        public float shortJumpInputDuration = 0.1f;
+        [SerializeField, Range(0, 1)]
+        public float mediumJumpInputDuration = 0.2f;
+        [SerializeField, Range(0, 1)]
+        public float shortJumpExecutionDuration = 0.25f;
+        [SerializeField, Range(0, 1)]
+        public float mediumJumpExecutionDuration = 0.5f;
+        [SerializeField, Range(0, 1)]
+        public float longJumpExecutionDuration = 0.75f;
 
         [Header("Look")]
         [SerializeField]
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -4,12 +4,22 @@ using UnityEngine.InputSystem;
 
 namespace Runtime.Player {
     public class Movement : IUpdatable, IDisposable {
+        enum JumpState {
+            NotJumping,
+            ShortJump,
+            MediumJump,
+            LongJump,
+        }
         AvatarSettings settings;
         AvatarInput.PlayerActions input;
         CharacterController character;
 
         public Vector3 velocity;
         Vector3 acceleration;
+
+        bool intendsToJump;
+        JumpState jumpState;
+        float jumpTimer;
 
         public Movement(AvatarSettings settings, AvatarInput.PlayerActions input, CharacterController character) {
             this.settings = settings;
@@ -38,6 +48,8 @@ namespace Runtime.Player {
         }
 
         public void Update(float deltaTime) {
+            ProcessJump();
+
             var movement = input.Movement.ReadValue<Vector2>();
             var targetVelocity = new Vector3(movement.x * settings.maxSpeed, velocity.y, movement.y * settings.maxSpeed);
             targetVelocity = character.transform.rotation * targetVelocity;
@@ -54,11 +66,58 @@ namespace Runtime.Player {
         }
 
         void HandleJumpStart(InputAction.CallbackContext context) {
+            /*
             if (character.isGrounded) {
                 velocity.y = settings.jumpSpeed;
+                jumpTimer = 0;
+                jumpState = JumpState.ShortJump;
+                intendsToJump = true;
             }
+            //*/
         }
         void HandleJumpCancel(InputAction.CallbackContext context) {
+            //intendsToJump = false;
+            //jumpState = JumpState.NotJumping;
+        }
+        void ProcessJump() {
+            switch (jumpState) {
+                case JumpState.NotJumping:
+                    if (character.isGrounded && input.Jump.phase == InputActionPhase.Started) {
+                        jumpTimer = 0;
+                        jumpState = JumpState.ShortJump;
+                        velocity.y = settings.jumpStartSpeed;
+                    }
+                    break;
+                case JumpState.ShortJump:
+                    jumpTimer += Time.deltaTime;
+                    if (jumpTimer >= settings.shortJumpInputDuration && input.Jump.phase == InputActionPhase.Started) {
+                        jumpState = JumpState.MediumJump;
+                    }
+                    break;
+                case JumpState.MediumJump:
+                    jumpTimer += Time.deltaTime;
+                    if (jumpTimer >= settings.mediumJumpInputDuration && input.Jump.phase == InputActionPhase.Started) {
+                        jumpState = JumpState.LongJump;
+                    }
+                    break;
+                case JumpState.LongJump:
+                    jumpTimer += Time.deltaTime;
+                    break;
+                default:
+                    break;
+            }
+            if (jumpState != JumpState.NotJumping) {
+                float duration = jumpState switch {
+                    JumpState.ShortJump => settings.shortJumpExecutionDuration,
+                    JumpState.MediumJump => settings.mediumJumpExecutionDuration,
+                    JumpState.LongJump => settings.longJumpExecutionDuration,
+                    _ => throw new NotImplementedException(),
+                };
+                if (jumpTimer >= duration) {
+                    jumpState = JumpState.NotJumping;
+                    velocity.y = Math.Min(velocity.y, settings.jumpStopSpeed);
+                }
+            }
         }
         void HandleSonarStart(InputAction.CallbackContext context) {
         }
