638e89a022757243b34b992d29df17e76e33ac24
Daniel Schulz <faulolio@gmail.com>
2021-07-10 18:08:30 +0200

    Tweak particles

--- Assets/Scripts/Player/ParticleComponent.cs
+++ Assets/Scripts/Player/ParticleComponent.cs
@@ -7,6 +7,8 @@ namespace Runtime.Player {
         ParticleSystem sonarSystem = default;
         [SerializeField]
         ParticleSystem paintSystem = default;
+        [SerializeField]
+        new Renderer renderer = default;
 
         List<ParticleCollisionEvent> events = new List<ParticleCollisionEvent>(1024);
         void OnParticleCollision(GameObject other) {
@@ -17,6 +19,15 @@ namespace Runtime.Player {
                     rotation3D = Quaternion.LookRotation(events[i].normal, Vector3.up).eulerAngles,
                 };
                 paintSystem.Emit(emitParams, 1);
+            }
+        }
+        void OnCollisionEnter(Collision collision) {
+            if (TryGetComponent<Rigidbody>(out var rigidbody)) {
+                rigidbody.isKinematic = true;
+                sonarSystem.Play();
+            }
+            if (renderer) {
+                renderer.enabled = false;
             }
         }
     }
